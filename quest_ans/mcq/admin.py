96356c7e77047905f2db697c30b8e5bd0190772c
shubham thakur <79434430+shubhamthakur19@users.noreply.github.com>
2022-01-20 17:34:35 +0530

    Delete quest_ans/mcq directory

--- quest_ans/mcq/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Question, Question_set, Options
-# Register your models here.
-admin.site.register(Question_set)
-admin.site.register(Question)
-admin.site.register(Options)
